38869bf2c8cf0a77b801207e906b6d7ef7b321d5
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-06-16 14:55:52 +0900

    add editer for user info

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ApplicationRecord
-  validates :picture, presence: true
+  validates :content, presence: true
   
   belongs_to :user
   has_many :favorites, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_validation { email.downcase!}
   has_secure_password
 
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, on: :create
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
