bec1c40a9f7d0184b953f605d31154d7c2425d6e
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-06-17 18:09:58 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy, :index]
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
